16f59c6c376df66695b82a9f7c4c209270624b8d
PonyMa <77607022@qq.com>
2020-12-24 10:00:47 +0000

    首页话题列表

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/','PagesController@root')->name('root');
+Route::get('/','TopicsController@index')->name('root');
 
 //Auth::routes();此条为自动生成，等同于下面路由
 //等同开始--------------
